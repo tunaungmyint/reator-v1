c6b47b2113700b6c588ddf8c283b51200ea02ee1
Tun Aung Myint <kakyi99999@gmail.com>
2022-11-17 20:01:34 +0630

    install reactjs and tailwindcss and create first template

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl text-purple-700">Hello Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
